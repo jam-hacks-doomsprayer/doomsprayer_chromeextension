caa1cc412aa8abf9fd684b8679dfdba967727ba9
jun li <junchenli423@gmail.com>
2025-05-17 22:36:31 -0400

    done chrome-extension

--- dist/background.js
+++ dist/background.js
@@ -23,7 +23,9 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 chrome.tabs.onActivated.addListener(activeInfo => {
   chrome.tabs.get(activeInfo.tabId, tab => {
+    console.log(tab);
     if (tab && tab.url) {
+      console.log('im here rn');
       handleUrl(tab.url);
     }
   });
@@ -33,18 +35,22 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     handleUrl(tab.url);
   }
 });
-function handleUrl(the_url) {
+async function handleUrl(the_url) {
   const extra_urls = ["https://www.reddit.com/?feed=home", "https://www.youtube.com", "https://www.tiktok.com"];
-  fetch("http://localhost:5000/scrape", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      extra_urls,
-      the_url
-    })
-  });
+  try {
+    await fetch("http://localhost:5000/scrape", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        extra_urls,
+        the_url
+      })
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 /******/ })()
 ;
